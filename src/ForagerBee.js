eaec1a895497b15040b930c2244afc2a3b9ef4be
Christopher Chen <chen1385@gmail.com>
2017-12-04 14:35:14 -0800

    finished ES5

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+	this.age = 10;
+	this.job = 'find pollen';
+	this.canFly = true;
+	//has own treasureChest array that masks prototype's treasureChest array
+	this.treasureChest = [];
 };
+
+
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function() {
+  this.treasureChest.push('treasure');
+}
+ForagerBee.prototype.treasureChest = [];
 
